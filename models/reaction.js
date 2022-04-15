9284d346b75ab463500777abb63e5d8b9533e4ce
hannah AB <hannah.allisonbatt@gmail.com>
2022-04-15 10:39:31 +0800

    connection done

--- models/reaction.js
+++ models/reaction.js
@@ -1,48 +1,4 @@
 const mongoose = require('mongoose');
 
-// Child documents or subdocuments can be embedded into a parent document
-// the managerSchema defines the shape for manager subdocument
-const userSchema = new mongoose.Schema({
-  userName: { type: String, required: true },
-  email: {type: String, required: true},
-  thoughts: [thoughtSchema],
-  friends:[], 
-});
-
-// The employeeScheme defines the shape for the employee subdocument
-const thoughtSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  salary: Number,
-});
-
-// departmentSchema provides the shape of the parent document
-const ReactionSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  // This will add a single subdocument to include the manager's information
-  reactionBody: managerSchema,
-  // This will include an array that holds all the employees' information
-  employees: [employeeSchema],
-  lastAccessed: { type: Date, default: Date.now },
-});
-
-// Uses mongoose.model() to create model
-const Reaction = mongoose.model('Department', ReactionSchema);
-
-// Uses model to create new instance including subdocument
-const managerData = { name: 'Taylor', salary: 80000 };
-const employeeData = [
-  { name: 'Ann', salary: 40000 },
-  { name: 'Liu', salary: 50000 },
-];
-
-Reaction.create(
-  { name: 'Shoes', manager: managerData, employees: employeeData },
-  (err, data) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(data);
-  }
-);
 
 module.exports = Department;
--- server.js
+++ server.js
@@ -1,33 +1,17 @@
 const express = require('express');
-const mongodb = require('mongodb').MongoClient;
-const data = require('./models/data');
 
+const db = require('./config/connection');
+//const routes = require('./routes');
+
+const PORT = process.env.port || 3001;
 const app = express();
-const port = 3001;
 
-const connectionStringURI = `mongodb://localhost:27017/groceryListDB`;
-
-let db;
-
-mongodb.connect(
-  connectionStringURI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    db = client.db();
-    // Drops any documents, if they exist
-    db.collection('socialNetwork').deleteMany({});
-    // Adds data to database
-    db.collection('socialNetwork').insertMany(data, (err, res) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(res.ops);
-    });
-
-    app.listen(port, () => {
-      console.log(`app listening at http://localhost:${port}`);
-    });
-  }
-);
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on http://localhost:${PORT}!`);
+  });
+});
